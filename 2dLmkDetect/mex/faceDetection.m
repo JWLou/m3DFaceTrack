564a43fd794a33a299b5bf49079486a9fd4a5284
JWLou <70638114+JWLou@users.noreply.github.com>
2020-09-02 17:18:53 +0100

    Add files via upload

---
+++ 2dLmkDetect/mex/faceDetection.m
@@ -0,0 +1,8 @@
+% Detect faces from a image uisng OpenCV Viola-Jones face detector
+% faces = faceDetection(img)
+% img: input image
+% faces: detected face rectangles
+
+% Dependence: OpenCV 2.4.13
+% Authors: Jianwen Lou
+% Creation Date: 22/11/2018
